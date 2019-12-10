c885961fe70d28dc16340b154b2d08b56d53a970
sonzza <sonzza.life@gmail.com>
2019-12-10 23:16:14 +0300

    Add JOIN for first exercise, LEFT JOIN for other.

--- HW_7.sql
+++ HW_7.sql
@@ -74,11 +74,14 @@ INSERT INTO `orders_products` (`id`, `order_id`, `product_id`, `total`, `created
 -- #1 Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 SELECT * FROM orders;
 DELETE FROM orders WHERE user_id = 4;
-SELECT id, name FROM users WHERE id IN(SELECT user_id FROM orders);
+SELECT id, name FROM users u WHERE id IN(SELECT user_id FROM orders);
+
+-- доработанное до JOIN;
+SELECT DISTINCT u.id, u.name FROM users u JOIN orders o WHERE u.id  = o.user_id;
 
 -- #2 Выведите список товаров products и разделов catalogs, который соответствует товару.
 SELECT p.name, c.*
-	FROM products p JOIN catalogs c
+	FROM products p LEFT JOIN catalogs c
 		ON c.id = p.catalog_id;
 	
 -- #3 (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). Поля from, to и label содержат английские названия городов, поле name — русское. Выведите список рейсов flights с русскими названиями городов.
@@ -100,7 +103,7 @@ INSERT INTO cities (`label`, `name`) VALUES ('moscow', 'Москва'), ('novgor
 
 
 SELECT c.name AS `откуда`, ci.name AS `куда` 
-	FROM cities c JOIN cities ci JOIN flights f 
+	FROM cities c LEFT JOIN cities ci LEFT JOIN flights f 
 		ON c.label = f.`from` AND ci.label = f.`to` 
 			ORDER BY FIELD (f.`id`, 1, 2, 3, 4, 5);
 
